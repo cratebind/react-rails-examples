5c64ea94b451c74368a8c0adb314a2ae0891be58
Iqra <35740252+iqra-javed@users.noreply.github.com>
2019-11-19 15:26:05 -0600

    Create locations route and display locations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :locations
   root to: 'home#index'
 
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
